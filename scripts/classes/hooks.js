7ca48cdc790983890b8d3e08fcd536396d057929
BuildTools <unconfigured@null.spigotmc.org>
2023-12-12 17:21:17 -0500

    Remove comment

--- scripts/classes/hooks.js
+++ scripts/classes/hooks.js
@@ -36,7 +36,7 @@ Hooks.on('renderActorSheet', (app, html, options) => {
             if(isOwner){
                 //We own the charcter so allow defense roll to occour
                 DEFFENDER_FORM_OBJ = new DefendCheckForm(event.currentTarget.attributes[1].value).render(true);
-                //DEFFENDER_FORM_OBJ.render(true);
+                
             }          
         });
     }
