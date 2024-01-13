61d69dfc958b921d07451d414d2829131a0ce6db
jacobwojoski <jacobwojoski@gmail.com>
2024-01-12 21:49:25 -0500

    fix formatting

--- scripts/classes/hooks.js
+++ scripts/classes/hooks.js
@@ -56,10 +56,10 @@ Hooks.on('renderActorSheet', (app, html, options) => {
             isOwner=true;
         }
 
-            if(isOwner){
-                //We own the charcter so allow defense roll to occour
-                DEFFENDER_FORM_OBJ = new DefendCheckForm(event.currentTarget.attributes[1].value).render(true);
-            }          
+        if(isOwner){
+            //We own the charcter so allow defense roll to occour
+            DEFFENDER_FORM_OBJ = new DefendCheckForm(event.currentTarget.attributes[1].value).render(true);
+        }          
     }); // End click callback
 
 });
